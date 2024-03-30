1418730d3d8735c0b3039fee35d0263abf67961c
Anna H00k <annahook0@gmail.com>
2024-03-30 16:21:26 +0100

    bigger text for menu buttons

--- app/components/menuHeader.tsx
+++ app/components/menuHeader.tsx
@@ -4,18 +4,18 @@ export default function MenuHeader() {
     <header>
       <nav>
         <div className="sm:flex sm:flex-row grid grid-cols-2 flex-col items-center justify-center sm:fixed absolute  py-4 px-2 gap-2 w-full">
-          <a href="/#projects" className="sm:text-lg text-[#A0A2A0] text-center text-sm rounded-full bg-[#303830] px-3 py-1">
+          <a href="/#projects" className="sm:text-lg text-[#A0A2A0] text-center text-md rounded-full bg-[#303830] px-3 py-1">
             Projects
           </a>
           <a
             href="/#techStack"
-            className="sm:text-lg text-sm rounded-full text-[#A0A2A0] text-center bg-[#303830] px-3 py-1"          >
+            className="sm:text-lg text-md rounded-full text-[#A0A2A0] text-center bg-[#303830] px-3 py-1"          >
             Tech Stack
           </a>
-          <a href="/#media" className="sm:text-lg text-sm text-[#A0A2A0] text-center rounded-full bg-[#303830] px-3 py-1">
+          <a href="/#media" className="sm:text-lg text-md text-[#A0A2A0] text-center rounded-full bg-[#303830] px-3 py-1">
             Media
           </a>
-          <a href="/#products" className="sm:text-lg text-sm text-[#A0A2A0] text-center rounded-full bg-[#303830] px-3 py-1">
+          <a href="/#products" className="sm:text-lg text-md text-[#A0A2A0] text-center rounded-full bg-[#303830] px-3 py-1">
             Products
           </a>
         </div>
