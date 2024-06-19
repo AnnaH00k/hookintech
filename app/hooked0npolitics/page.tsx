fe229a431086f3028e5789008b354f78ce1d54a1
Anna H00k <annahook0@gmail.com>
2024-06-19 21:34:20 +0200

    fix error

---
+++ app/hooked0npolitics/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import MenuHeader from "../components/menuHeader";
+import Footer from "../components/footer";
+import Script from "next/script";
+import Link from "next/link";
+
+export default function Hooked0nPolitics() {
+  return (
+    <main className="flex flex-col items-center min-h-screen justify-between py-20 bg-[#4e2532] w-full">
+
+
+  </main>
+  );
+}
