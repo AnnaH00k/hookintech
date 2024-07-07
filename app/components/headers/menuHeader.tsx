54e70845e6f007cbdf0af3d036e6a98cd3b1a947
AnnaH00k <90142429+AnnaH00k@users.noreply.github.com>
2024-07-07 08:11:20 +0200

    header layout

--- app/components/headers/menuHeader.tsx
+++ app/components/headers/menuHeader.tsx
@@ -62,7 +62,7 @@ export default function MenuHeader() {
           <a href="/#products" className="sm:text-lg text-md text-[#A0A2A0] text-center rounded-full bg-[#303830] px-3 py-1">
             Products
           </a>
-          <a href="/blogposts" className="col-span-2 sm:text-lg text-md text-[#A0A2A0] text-center rounded-full bg-[#303830] px-3 py-1">
+          <a href="/blogposts" className=" sm:text-lg text-md text-[#A0A2A0] text-center rounded-full bg-[#303830] px-3 py-1">
             Blogposts
           </a>
           
