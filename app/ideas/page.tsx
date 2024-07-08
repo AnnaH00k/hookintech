e00a074661ad3206b37fba9a5e054d90589a9046
AnnaH00k <90142429+AnnaH00k@users.noreply.github.com>
2024-07-08 09:45:37 +0200

    text betterment

--- app/ideas/page.tsx
+++ app/ideas/page.tsx
@@ -70,7 +70,7 @@ export default function Ideas() {
     return (
       <main className="flex flex-col items-center min-h-screen justify-center sm:p-20  sm:mt-0 py-5 text-[#A0A2A0] bg-[#070b0b] w-full">
         <h1 className="sm:text-5xl text-xl font-bold mt-[5vh] mb-10">Meine Ideen</h1>
-        <table className="text-xs sm:text-md w-[90vw] max-w-5xl">
+        <table className="text-xs sm:text-lg w-[90vw] max-w-5xl">
           <thead>
             <tr>
               <th className="sm:px-4 px-1 sm:py-2 py-1 border border-[#303830]">App Name</th>
