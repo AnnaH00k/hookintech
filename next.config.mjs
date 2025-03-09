6139fe4cfd852de58acabf4bd796970d222e0912
Anna H00k <annahook0@gmail.com>
2025-03-09 16:32:16 +0100

    fix mjs

--- next.config.mjs
+++ next.config.mjs
@@ -5,26 +5,25 @@ const basePath = isProduction ? `/${repoName}` : "";
 
 const nextConfig = {
   output: "export",
-  basePath,
+  basePath: basePath,
   assetPrefix: basePath,
   images: {
     unoptimized: true,
     loader: "custom",
-    loaderFile: "./utils/imageLoader.ts",
+    loaderFile: "./utils/imageLoader.ts"
   },
   experimental: {
-    serverComponentsExternalPackages: ["gray-matter"],
+    serverComponentsExternalPackages: ["gray-matter"]
   },
   webpack: (config, { isServer }) => {
     if (!isServer) {
-      // Don't include fs module on the client side
       config.resolve.fallback = {
         fs: false,
-        path: false,
+        path: false
       };
     }
     return config;
-  },
+  }
 };
 
 export default nextConfig;
